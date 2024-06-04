48172a721c568e2c0177da88480c992eeaa426cd
Gabriel Rodrigues <gabers357@gmail.com>
2024-06-03 22:32:07 -0300

    chore: Update Tailwind CSS configuration and dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from 'daisyui';
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/daisyui/dist/**/*.js",
+    "node_modules/react-daisyui/dist/**/*.js",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
-}
-
+  plugins: [require("daisyui")],
+};
