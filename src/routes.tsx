926080332152ca1480aed6896cfbedf029831d46
Gabriel Rodrigues <gabers357@gmail.com>
2024-06-03 22:32:18 -0300

    feat: Add Navbar component and update routes for series, live, and movies

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,35 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
 
-const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
+const router = createBrowserRouter([
+  {
+    path: "/series",
+    element: (
+      <>
+        <Navbar />
+        <Home />
+      </>
+    ),
+  },
+  {
+    path: "/live",
+    element: (
+      <>
+        <Navbar />
+        <Home />
+      </>
+    ),
+  },
+  {
+    path: "/movies",
+    element: (
+      <>
+        <Navbar />
+        <Home />
+      </>
+    ),
+  },
+]);
 
 export default router;
