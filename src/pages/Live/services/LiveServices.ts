2f18ea27d2dff380b4159f1c16590bae4f83863c
Gabriel Rodrigues <gabers357@gmail.com>
2024-06-04 23:43:57 -0300

    refactor: Update resolveFinalUrl function in LiveServices.ts to handle redirect locations

--- src/pages/Live/services/LiveServices.ts
+++ src/pages/Live/services/LiveServices.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 export const resolveFinalUrl = async (url: string): Promise<string> => {
   try {
     const proxyUrl = `/api${new URL(url).pathname}`;
-    const response = await axios.head(proxyUrl, { maxRedirects: 5 });
-    return response.request.responseURL;
+    const response = await axios.get(proxyUrl, { maxRedirects: 5 });
+    return response.request.responseURL || response.config.url;
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     if (
@@ -12,10 +12,12 @@ export const resolveFinalUrl = async (url: string): Promise<string> => {
       error.response.status >= 300 &&
       error.response.status < 400
     ) {
-      return error.response.headers.location;
-    } else {
-      console.error("Error resolving final URL:", error);
-      return url;
+      const location = error.response.headers.location;
+      if (location) {
+        return resolveFinalUrl(location);
+      }
     }
+    console.error("Error resolving final URL:", error);
+    return url;
   }
 };
