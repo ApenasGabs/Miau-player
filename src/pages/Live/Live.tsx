2ddebda98bd9c240f1447b4b0c2cc43cbb72538e
Gabriel Rodrigues <gabers357@gmail.com>
2024-06-04 21:09:03 -0300

    refactor: Update Live component layout

--- src/pages/Live/Live.tsx
+++ src/pages/Live/Live.tsx
@@ -1,5 +1,11 @@
 const Live = () => {
-  return <div>Live</div>;
+  return (
+    <div className=" hidden lg:flex flex-row w-full">
+      <div className="w-1/4">Categoria</div>
+      <div className="w-2/6">Ao vivo</div>
+      <div className="w-3/5">Video</div>
+    </div>
+  );
 };
 
 export default Live;
